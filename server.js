175c44d2d59517edca974c884c2eba4f577f156c
Rutuja Sontakke <rutujasontakke50@gmail.com>
2025-09-03 20:12:42 +0530

    Updated project file

--- server.js
+++ server.js
@@ -16,12 +16,33 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+let users = [];
 
 app.post("/submit", (req, res) => {
-    res.render("result", { data: req.body });
+    const {name, email, password} = req.body;
+
+    if(!name || name.length < 3) {
+        return res.send("Invalid name");
+    }
+   
+    if(!email.includes("@")) {
+        return res.send("Invalid email");
+    }
+   
+    if(password.length < 6) {
+        return res.send("Password is too short");
+    }
+   
+    if(password !== confirmpassword) {
+        return res.send("Password do not match");
+    }
+
+    users.push({name, email, password }); //storing data in array for better 
+
+    return res.send("Registered Successfully!")
 });
+
 
 app.listen(8080, () => {
     console.log("Server is listerning on port 8080");
 });
-
